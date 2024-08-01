19482de9bb3857b7b5cec0b79b15e6d00a6087a1
VDanchev <viktordan315@gmail.com>
2024-08-01 16:28:31 +0300

    Added new authentication controller

---
+++ HealthSync.Server/Controllers/AuthenticationController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthSync.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+    }
+}
