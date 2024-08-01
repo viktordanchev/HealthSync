9217da2e033de4176bd061ab5f4fc26e2ad466ff
VDanchev <viktordan315@gmail.com>
2024-08-01 16:28:08 +0300

    initialized database context

---
+++ Infrastructure/HealthSyncDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class HealthSyncDbContext : IdentityDbContext<IdentityUser>
+    {
+        public HealthSyncDbContext(DbContextOptions<HealthSyncDbContext> options) :
+            base(options)
+        { }
+    }
+}
