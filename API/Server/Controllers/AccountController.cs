8fb5f4aeaf34a2bcdc73e77672b691fdab83c11f
VDanchev <viktordan315@gmail.com>
2024-08-07 16:42:46 +0300

    Renamed

---
+++ API/Server/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Server.Models;
+
+namespace HealthSync.Server.Controllers
+{
+    [Route("account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public IActionResult Register([FromBody] UserRegister userRegister)
+        {
+            if (User.Identity.IsAuthenticated) 
+            {
+                return Redirect("https://localhost:7080/weatherForecast");
+            }
+
+
+            return BadRequest();
+        }
+    }
+}
