d4f6dbd637171a99124d31d8aeb04fbfa0a89ff4
VDanchev <viktordan315@gmail.com>
2024-08-07 16:41:44 +0300

    Created new project for all constants

---
+++ Common/Entities/ApplicationUserConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Common.EntityConstants
+{
+    public static class ApplicationUserConstants
+    {
+        public const string NameMatch = @"^[A-Z][a-z]+$";
+        public const int UNCLength = 10;
+    }
+}
