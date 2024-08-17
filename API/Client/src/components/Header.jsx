0b32097c077d99e57491ff22613d924088f0a50f
VDanchev <viktordan315@gmail.com>
2024-08-17 14:19:52 +0300

    Added new component

---
+++ API/Client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+
+    return (
+        <header>
+            <a href="#" class="text-3xl">
+                Logo
+            </a>
+        </header>
+    );
+}
+
+export default Header;
