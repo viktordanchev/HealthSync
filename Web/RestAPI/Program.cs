f8fe8e84088352eb6cb8ea7eb9270d1d4c1ea5b1
VDanchev <viktordan315@gmail.com>
2024-09-04 15:49:56 +0300

    Updated

--- Web/RestAPI/Program.cs
+++ Web/RestAPI/Program.cs
@@ -3,7 +3,6 @@ using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,7 +16,6 @@ var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -30,13 +28,5 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-app.UseCors(x =>
-{
-    x.WithOrigins("https://localhost:5173");
-    x.AllowAnyHeader();
-    x.AllowAnyMethod();
-});
-
-app.MapFallbackToFile("/index.html");
 
 app.Run();
