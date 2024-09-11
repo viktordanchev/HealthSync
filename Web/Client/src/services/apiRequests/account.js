85e49af1feb76c45421c4b94a3c7b164997daede
Viktor Danchev <viktordan315@gmail.com>
2024-09-11 21:35:56 +0300

    Separated API requests

---
+++ Web/Client/src/services/apiRequests/account.js
@@ -0,0 +1,29 @@
+const url = 'https://localhost:7080/api/account';
+
+export const login = async (values) =>
+    await fetch(`${url}/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(values),
+        credentials: 'include'
+    });
+
+export const register = async (values) =>
+    await fetch(`${url}/register`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(values)
+    });
+
+export const isJWTTokenExpired = async () =>
+    await fetch(`${url}/isTokenExpired`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credentials: 'include'
+    });
