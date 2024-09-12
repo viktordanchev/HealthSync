679090aa881fcc05e8f4d822c011c3d4c6499078
Viktor Danchev <viktordan315@gmail.com>
2024-09-12 21:42:44 +0300

    Added token service to handle jwt tokens

---
+++ Core/Services/Contracts/ITokenService.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Entities;
+using System.Security.Claims;
+
+namespace Core.Services.Contracts
+{
+    public interface ITokenService
+    {
+        string GenerateRefreshToken(IEnumerable<Claim> claims);
+        string GenerateAccessToken(IEnumerable<Claim> claims);
+    }
+}
---
+++ Core/Services/TokenService.cs
@@ -0,0 +1,57 @@
+﻿using Core.Services.Contracts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Core.Services
+{
+    public class TokenService : ITokenService 
+    {
+        private IConfiguration _configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// Generate JWT refresh token.
+        /// </summary>
+        public string GenerateRefreshToken(IEnumerable<Claim> claims)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var expireTime = int.Parse(_configuration["CookieSettings:RefreshJWTTokenMonths"]);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(5),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        /// <summary>
+        /// Generate JWT access token.
+        /// </summary>
+        public string GenerateAccessToken(IEnumerable<Claim> claims)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var expireTime = int.Parse(_configuration["CookieSettings:AccessJWTTokenMinutes"]);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(1),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
