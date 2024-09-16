8ab63848e11e3de41185895f0acb9e9068eb140d
Viktor Danchev <viktordan315@gmail.com>
2024-09-16 19:18:31 +0300

    Added new component for loading

---
+++ Web/Client/src/components/Loading.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Loading() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div>
+                <p className="text-xl font-bold">Loading</p>
+                <div className="mt-4 flex justify-center">
+                    <div className="w-14 h-14 border-4 border-maincolor border-dotted rounded-full animate-spin"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Loading;
