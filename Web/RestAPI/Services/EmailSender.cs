168662b2fe8451aa3371a62d8f6aa2b9428d9de6
Viktor Danchev <viktordan315@gmail.com>
2024-09-18 18:54:58 +0300

    Added EmailSender service

---
+++ Web/RestAPI/Services/EmailSender.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace RestAPI.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly IConfiguration _config;
+
+        public EmailSender(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string htmlMessage)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_config["EmailSettings:From"]));
+            email.To.Add(MailboxAddress.Parse(toEmail));
+            email.Subject = subject;
+            email.Body = new TextPart("html") { Text = htmlMessage };
+
+            using var smtp = new SmtpClient();
+            await smtp.ConnectAsync(_config["EmailSettings:SmtpServer"], int.Parse(_config["EmailSettings:Port"]), MailKit.Security.SecureSocketOptions.StartTls);
+            await smtp.AuthenticateAsync(_config["EmailSettings:Username"], _config["EmailSettings:Password"]);
+            await smtp.SendAsync(email);
+            await smtp.DisconnectAsync(true);
+        }
+    }
+}
