e1d1e609c5732264a79048a0dae0cf0accd17977
Viktor Danchev <viktordan315@gmail.com>
2024-09-19 17:39:41 +0300

    Added new service to manage the verification of registered users

---
+++ Web/RestAPI/Services/Contracts/IVerificationCodeService.cs
@@ -0,0 +1,8 @@
+﻿namespace RestAPI.Services.Contracts
+{
+    public interface IVerificationCodeService
+    {
+        string GenerateCode(string key);
+        bool ValidateCode(string key, string vrfCode);
+    }
+}
---
+++ Web/RestAPI/Services/VerificationCodeService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using RestAPI.Services.Contracts;
+
+namespace RestAPI.Services
+{
+    public class VerificationCodeService : IVerificationCodeService
+    {
+        private IMemoryCache _memoryCache;
+
+        public VerificationCodeService(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public string GenerateCode(string key)
+        {
+            var vrfCode = Guid.NewGuid().ToString().Substring(0, 6);
+            _memoryCache.Set(key, vrfCode, new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(1)
+            });
+
+            return vrfCode;
+        }
+
+        public bool ValidateCode(string key, string vrfCode)
+        {
+            var code = _memoryCache.Get(key);
+
+            return code != null && code.ToString() == vrfCode ? true : false;
+        }
+    }
+}
