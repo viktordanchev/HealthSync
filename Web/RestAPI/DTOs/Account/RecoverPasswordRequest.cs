8cbb5d2f493e79e7cc89d7ddb90e3a1ff25f4ab1
Viktor Danchev <viktordan315@gmail.com>
2024-09-23 21:09:50 +0300

    Added new dto

---
+++ Web/RestAPI/DTOs/Account/RecoverPasswordRequest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static Common.Errors.Account;
+
+namespace RestAPI.DTOs.Account
+{
+    public class RecoverPasswordRequest
+    {
+        [Required]
+        public string Token { get; set; } = null!;
+
+        [Required(ErrorMessage = $"Password {RequiredField}")]
+        public string Password { get; set; } = null!;
+
+        [Required(ErrorMessage = $"Confirm password {RequiredField}")]
+        [Compare("Password", ErrorMessage = PasswordMatch)]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
