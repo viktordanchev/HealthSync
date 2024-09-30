1683b67fe915eda450a8b37859b9a12da8c34e0e
Viktor Danchev <viktordan315@gmail.com>
2024-09-30 18:41:49 +0300

    Added more errors and messages

--- Common/Messages/Account.cs
+++ Common/Messages/Account.cs
@@ -3,5 +3,6 @@
     public static class Account
     {
         public const string NewVrfCode = "New verification code is sended.";
+        public const string SendedPassRecoverLink = "Recover password link was sended to your email.";
     }
 }
