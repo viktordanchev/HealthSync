ab16c50de60e1330c4ae010bab221d33b38a90d2
Viktor Danchev <viktordan315@gmail.com>
2024-10-04 19:07:47 +0300

    Added new entity

---
+++ Infrastructure/Entities/Doctor.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Infrastructure.Entities
+{
+    class Doctor
+    {
+        [Required]
+        public string IdenitityId { get; set; } = null!;
+
+        [ForeignKey(nameof(IdenitityId))]
+        public ApplicationUser Identity { get; set; } = null!;
+    }
+}
