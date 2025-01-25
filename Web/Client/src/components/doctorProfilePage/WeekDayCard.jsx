8cdfa500f4f9d888ce1d31cd84678d0fdde08ad7
Viktor Danchev <viktordan315@gmail.com>
2025-01-25 18:24:55 +0200

    Updated components

--- Web/Client/src/components/doctorProfilePage/WeekDayCard.jsx
+++ Web/Client/src/components/doctorProfilePage/WeekDayCard.jsx
@@ -6,38 +6,30 @@ function WeekDayCard({ data }) {
     const [minutes, setMinutes] = useState(data.meetingTimeMinutes);
 
     return (
-        <div className="w-56 bg-zinc-700 bg-opacity-45 p-3 rounded-xl shadow-xl shadow-gray-400">
-            <label className="inline-flex items-center cursor-pointer">
-                <input
-                    className="sr-only peer"
-                    type="checkbox"
-                    checked={isWorkDay}
-                    onChange={(e) => setIsWorkDay(e.target.checked)}
-                />
-                <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-red-500 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-500"></div>
-            </label>
-            <div className="space-y-3">
-                <p className="text-center text-xl">{data.weekDay}</p>
-                <div className="flex justify-between">
-                    <label className="text-base font-medium text-gray-800">Meeting minutes:</label>
-                    <input
-                        className="w-1/3 px-1 rounded text-gray-800 border-2 border-zinc-700 focus:outline-none focus:shadow-lg focus:shadow-gray-400 focus:border-maincolor"
-                        type="number"
-                        value={minutes}
-                        onChange={(e) => setMinutes(e.target.value)}
-                    />
+        <div className="p-2 bg-maincolor border border-zinc-500 rounded-xl text-white">
+            <div className="flex justify-between space-x-3 text-gray-700 font-medium">
+                <div className="flex items-center space-x-2">
+                    <input className="w-4 h-4 cursor-pointer"
+                        type="checkbox"
+                        checked={isWorkDay}
+                        onChange={() => setIsWorkDay(!isWorkDay)} />
+                    <label className="text-lg">{data.weekDay}</label>
                 </div>
-                <div className="text-center font-medium">
-                    <label className="text-base text-gray-800">Working time</label>
-                    <div className="flex space-x-2">
-                        <div className="w-full">
-                            <p>Start</p>
-                            <Timepicker time={data.workDayStart} />
-                        </div>
-                        <div className="w-full">
-                            <p>End</p>
-                            <Timepicker time={data.workDayEnd} />
-                        </div>
+                <div className="flex space-x-3">
+                    <div className="flex flex-col">
+                        <p className="text-center">Start</p>
+                        <Timepicker time={data.workDayStart} />
+                    </div>
+                    <div className="flex flex-col">
+                        <p className="text-center">End</p>
+                        <Timepicker time={data.workDayEnd} />
+                    </div>
+                    <div className="flex flex-col">
+                        <p className="text-center">Minutes</p>
+                        <input className="rounded-xl px-2 w-20 h-full text-gray-700 text-lg font-bold border-2 border-white appearance-none focus:outline-none focus:border-2 focus:border-blue-500"
+                            type="number"
+                            value={minutes}
+                            onChange={(e) => setMinutes(e.target.value)} />
                     </div>
                 </div>
             </div>
--- Web/Client/src/components/doctorProfilePage/WeeklySchedule.jsx
+++ Web/Client/src/components/doctorProfilePage/WeeklySchedule.jsx
@@ -3,12 +3,14 @@ import WeekDayCard from './WeekDayCard';
 
 function WeeklySchedule({ weekDays }) {
     return (
-        <div className="space-y-6 w-2/3">
-            <h2 className="text-center text-3xl font-thin underline-thin">Weekly Schedule</h2>
-            <div className="text-white flex justify-center items-center flex-wrap gap-3">
-                {weekDays.map((weekDay) => (
-                    <WeekDayCard key={weekDay.id} data={weekDay} />
-                ))}
+        <div className="flex justify-between items-center space-x-6">
+            <div className="space-y-4">
+                <h2 className="text-center text-3xl font-thin underline-thin text-gray-700">Weekly Schedule</h2>
+                <div className="space-y-2">
+                    {weekDays.map((weekDay) => (
+                        <WeekDayCard key={weekDay.id} data={weekDay} />
+                    ))}
+                </div>
             </div>
         </div>
     );
