a13809bc90283019fc03cbef4a917d8959925116
Viktor Danchev <viktordan315@gmail.com>
2025-02-15 19:00:34 +0200

    Updated

--- Client/src/components/doctorProfilePage/DaysoffCalendar.jsx
+++ Client/src/components/doctorProfilePage/DaysoffCalendar.jsx
@@ -3,13 +3,12 @@ import React, { useState, useEffect } from 'react';
 function DaysoffCalendar() {
     const [days, setDays] = useState([]);
     const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
-    const currentYear = new Date().getFullYear();
-
+    const [daysOff, setDaysOff] = useState([]);
     const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-
+    
     useEffect(() => {
-        generateDays(currentMonth, currentYear);
+        generateDays(currentMonth, new Date().getFullYear());
     }, [currentMonth]);
 
     const generateDays = (month, year) => {
@@ -46,11 +45,19 @@ function DaysoffCalendar() {
         }
     };
 
+    const handleSelectDate = (date) => {
+        setDaysOff(prev =>
+            prev.includes(date)
+                ? prev.filter(d => d !== date)
+                : [...prev, date]
+        );     
+    };
+
     return (
         <div className="h-80">
             <div className="h-1/6 flex items-center rounded-t-xl justify-evenly bg-maincolor font-medium py-1">
                 <button className="w-1/3" onClick={handlePreviousMonth}>Previous</button>
-                <p className="w-1/3">{monthNames[currentMonth]}</p>
+                <p className="w-1/3 text-lg">{monthNames[currentMonth]}</p>
                 <button className="w-1/3" onClick={handleNextMonth}>Next</button>
             </div>
             <div className="h-5/6 rounded-b-xl bg-white grid grid-cols-7 gap-2 text-center text-sm p-2">
@@ -58,15 +65,15 @@ function DaysoffCalendar() {
                     <div key={index}>{day}</div>
                 ))}
                 {days.map((day, index) => (
-                    <div className={`rounded-full flex items-center justify-center 
-                    ${day ? 'cursor-pointer hover:bg-gray-300' : 'opacity-45 cursor-default'}
-                    ${day && new Date().getDate() === day.date.getDate() &&
-                            new Date().getFullYear() === currentYear &&
-                            new Date().getMonth() === currentMonth
-                            ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
-                        key={index}>
-                        <p>{day ? day.date.getDate() : ''}</p>
-                    </div>
+                    <p
+                        className={`rounded-full flex items-center justify-center 
+                    ${day ? 'cursor-pointer' : 'opacity-45 cursor-default'}
+                    ${day && daysOff.includes(day.date.getTime()) ? 'bg-blue-500 hover:bg-blue-300 text-white' : 'bg-gray-200 hover:bg-gray-300'}`}
+                        key={index}
+                        onClick={() => handleSelectDate(day.date.getTime())}
+                    >
+                        {day ? day.date.getDate() : ''}
+                    </p>
                 ))}
             </div>
         </div>
