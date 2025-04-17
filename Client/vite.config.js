9c5f3c8d8f47b951256fc0202bf5f76b13a0e82f
Viktor Danchev <viktordan315@gmail.com>
2025-04-17 20:04:33 +0300

    Updated

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,9 +3,5 @@ import plugin from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
-    plugins: [plugin(), tailwindcss()],
-    build: {
-        outDir: 'dist',
-    },
-    base: '/'
+    plugins: [plugin(), tailwindcss()]
 })
