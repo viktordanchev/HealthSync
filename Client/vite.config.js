096113974f4a167cfb10c21843758b2807f10c03
Viktor Danchev <viktordan315@gmail.com>
2025-04-17 19:46:33 +0300

    Updated

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,13 +4,8 @@ import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
     plugins: [plugin(), tailwindcss()],
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        allowedHosts: [
-            'healthsync-client.up.railway.app',
-            'localhost',
-            '0.0.0.0'
-        ]
-    }
+    build: {
+        outDir: 'dist',
+    },
+    base: '/'
 })
