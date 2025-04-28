b0797a4abf3971f65e85fb87e08000c822e75ed1
Viktor Danchev <viktordan315@gmail.com>
2025-04-28 18:28:42 +0300

    Updated

--- Client/src/components/Message.jsx
+++ Client/src/components/Message.jsx
@@ -4,7 +4,7 @@ function Message({ message, type }) {
     return (
         <>
             {message &&
-                (<article className="fixed top-6 flex z-50 flex-col items-center sm:left-3 sm:top-3 sm:right-3">
+                (<article className="fixed top-6 left-6 flex z-50 flex-col items-center sm:left-3 sm:top-3 sm:right-3">
                 <div className={`text-center text-xl border border-zinc-500 ${type == 'message' ? 'bg-green-500' : 'bg-red-500'} rounded-xl px-6 py-3 space-y-3`}>
                         <p className="text-white">{message}</p>
                     </div>
